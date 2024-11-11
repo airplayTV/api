8da3386f4b9e7adbd7eba0d69e12449c9dd87a32
game.desktop <game.desktop@example.com>
2024-11-11 11:54:50 +0800

    修改工具方法

--- util/json.go
+++ util/json.go
@@ -2,8 +2,14 @@ package util
 
 import "encoding/json"
 
-func ToString(v any) string {
-	buff, err := json.MarshalIndent(v, "", "\t")
+func ToString(v any, pretty ...bool) string {
+	var buff []byte
+	var err error
+	if len(pretty) > 0 && pretty[0] == true {
+		buff, err = json.MarshalIndent(v, "", "\t")
+	} else {
+		buff, err = json.Marshal(v)
+	}
 	if err != nil {
 		return ""
 	}
