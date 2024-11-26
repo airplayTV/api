acd48f623d84bb932cf20a26a2d3c18257d414de
example <username@example.com>
2024-11-26 19:18:35 +0800

    没用啊

--- controller/timer.go
+++ controller/timer.go
@@ -3,19 +3,18 @@ package controller
 import (
 	"github.com/airplayTV/api/handler"
 	"log"
-	"time"
 )
 
 func init() {
-	go func() {
-		log.Println("[TimerStart]")
-		var t = time.NewTicker(time.Second * 60 * 5)
-		for range t.C {
-			for _, h := range sourceMap {
-				go holdCookie(h.Handler)
-			}
-		}
-	}()
+	//go func() {
+	//	log.Println("[TimerStart]")
+	//	var t = time.NewTicker(time.Second * 60 * 5)
+	//	for range t.C {
+	//		for _, h := range sourceMap {
+	//			go holdCookie(h.Handler)
+	//		}
+	//	}
+	//}()
 }
 
 func holdCookie(h handler.IVideo) {
