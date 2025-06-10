d124ff270b406bfbf6fedb8ad33dd23e4c040ce1
game.desktop <game.desktop@example.com>
2025-06-10 14:06:15 +0800

    添加/

--- controller/home.go
+++ controller/home.go
@@ -1,7 +1,6 @@
 package controller
 
 import (
-	"github.com/airplayTV/api/model"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
@@ -10,7 +9,5 @@ type HomeController struct {
 }
 
 func (x HomeController) Index(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, model.NewSuccess(map[string]interface{}{
-		"header": ctx.Request.Header,
-	}))
+	ctx.String(http.StatusOK, "AirplayTV")
 }
