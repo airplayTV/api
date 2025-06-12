9fd5c0702c5854d5769719f6bb83f6ff70226138
game.desktop <game.desktop@example.com>
2025-06-12 17:04:08 +0800

    添加源

--- model/cms-api-config.go
+++ model/cms-api-config.go
@@ -1,9 +1,10 @@
 package model
 
 type CmsApiConfig struct {
-	Id      string `mapstructure:"id"`
-	Name    string `mapstructure:"name"`
-	Host    string `mapstructure:"host"`
-	Api     string `mapstructure:"api"`
-	Disable bool   `mapstructure:"disable"`
+	Id         string `mapstructure:"id"`
+	Name       string `mapstructure:"name"`
+	Host       string `mapstructure:"host"`
+	Api        string `mapstructure:"api"`
+	Disable    bool   `mapstructure:"disable"`
+	Searchable bool   `mapstructure:"searchable"` // 是否可搜索
 }
