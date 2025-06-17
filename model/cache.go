8c704bde119a1ccc6fb9cae52a3215b9bc740d2a
game.desktop <game.desktop@example.com>
2025-06-17 18:00:45 +0800

    保存代码

---
+++ model/cache.go
@@ -0,0 +1,33 @@
+package model
+
+import (
+	"github.com/dgraph-io/ristretto"
+	"github.com/eko/gocache/lib/v4/cache"
+	ristretto_store "github.com/eko/gocache/store/ristretto/v4"
+)
+
+var cacheManager *cache.Cache[any]
+
+func CacheManager() *cache.Cache[any] {
+	if cacheManager != nil {
+		return cacheManager
+	}
+
+	// https://github.com/dgraph-io/ristretto
+	ristrettoCache, err := ristretto.NewCache(&ristretto.Config{
+		NumCounters: 10000,
+		MaxCost:     (1 << 30) / 2, // 512MB???
+		BufferItems: 64,
+		// NumCounters: 1e7,     // number of keys to track frequency of (10M).
+		// MaxCost:     1 << 30, // maximum cost of cache (1GB).
+		// BufferItems: 64,      // number of keys per Get buffer.
+	})
+	if err != nil {
+		panic(err)
+	}
+	ristrettoStore := ristretto_store.NewRistretto(ristrettoCache)
+
+	cacheManager = cache.New[any](ristrettoStore)
+
+	return cacheManager
+}
