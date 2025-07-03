3eacb9829d52c400f81c836f37e40212ed089a84
game.desktop <game.desktop@example.com>
2025-07-03 10:47:18 +0800

    保存代码

--- controller/source.go
+++ controller/source.go
@@ -24,18 +24,22 @@ func init() {
 			Name:       handler.CzzyHandler{}.Name(),
 			Searchable: true,
 		})},
-		//handler.SubbHandler{}.Name():     {Sort: 2, Handler: handler.SubbHandler{}.Init(nil)}, // 限制国内IP访问
+		//handler.SubbHandler{}.Name(): {Sort: 2, Handler: handler.SubbHandler{}.Init(model.CmsZyOption{
+		//	Id:         "subaibai",
+		//	Name:       handler.SubbHandler{}.Name(),
+		//	Searchable: true,
+		//})}, // 限制国内IP访问
 		//handler.YingshiHandler{}.Name():  {Sort: 3, Handler: handler.YingshiHandler{}.Init(nil)}, // api挂了
-		handler.MaYiHandler{}.Name(): {Sort: 4, Handler: handler.MaYiHandler{}.Init(model.CmsZyOption{
-			Id:         "mayi",
-			Name:       handler.MaYiHandler{}.Name(),
-			Searchable: true,
-		})},
-		handler.NaifeiMeHandler{}.Name(): {Sort: 5, Handler: handler.NaifeiMeHandler{}.Init(model.CmsZyOption{
-			Id:         "naifeigc",
-			Name:       handler.NaifeiMeHandler{}.Name(),
-			Searchable: true,
-		})},
+		//handler.MaYiHandler{}.Name(): {Sort: 4, Handler: handler.MaYiHandler{}.Init(model.CmsZyOption{
+		//	Id:         "mayi",
+		//	Name:       handler.MaYiHandler{}.Name(),
+		//	Searchable: true,
+		//})},
+		//handler.NaifeiMeHandler{}.Name(): {Sort: 5, Handler: handler.NaifeiMeHandler{}.Init(model.CmsZyOption{
+		//	Id:         "naifeigc",
+		//	Name:       handler.NaifeiMeHandler{}.Name(),
+		//	Searchable: true,
+		//})},
 		//handler.MeiYiDaHandler{}.Name():  {Sort: 6, Handler: handler.MeiYiDaHandler{}.Init(nil)}, // 不可达
 		//handler.Huawei8Handler{}.Name():  {Sort: 7, Handler: handler.Huawei8Handler{}.Init()},
 		//handler.Huawei8ApiHandler{}.Name(): {Sort: 8, Handler: handler.Huawei8ApiHandler{}.Init()},
